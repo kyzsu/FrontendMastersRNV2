05d7ec32aa497d360eef994a3f5bc84875109e3a
kyzsu <l0calh34rtz@gmail.com>
2021-10-22 02:15:53 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import ColorBox from './components/ColorBox';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Here are some boxes of different colours.
+        </Text>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#cb4b16" />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
+
+export default App;
